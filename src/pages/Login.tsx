c8bb5bd870fe1104600b6f0f663e1ea0ab324607
Sagar6628 <chandusagar628@gmail.com>
2026-02-08 16:31:41 +0530

    initial push

---
+++ src/pages/Login.tsx
@@ -0,0 +1,7 @@
+export const Login = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-amber-500 ">
+      <h1 className="text-4xl font-bold">Login</h1>
+    </div>
+  )
+}
